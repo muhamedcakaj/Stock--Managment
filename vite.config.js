9490178eaf0a6f3242838625d4c8973dae560c86
muhamedcakaj <muhamedcakaj57@gmail.com>
2026-03-15 14:49:10 +0100

    New

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // listen on all network interfaces
-    port: 5173,
-    strictPort: true,
-    allowedHosts: [
-      'localhost',
-      '127.0.0.1',
-      '192.168.0.13', // your PC LAN IP
-      'ichnological-maddeningly-juelz.ngrok-free.dev' // allow this ngrok host
-    ]
-  }
+  base: '/', // make sure the base is root
+  build: {
+    outDir: 'dist', // default, the folder Render will publish
+  },
 })
